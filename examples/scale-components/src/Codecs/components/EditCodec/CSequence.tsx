f278cf04df166b29e2f564f3d299c3c96980665c
Hannah Redler <hannahjredler@gmail.com>
2024-10-01 11:20:38 +0200

    Adding a drag and drop to sequence

--- examples/scale-components/src/Codecs/components/EditCodec/CSequence.tsx
+++ examples/scale-components/src/Codecs/components/EditCodec/CSequence.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { EditSequence, NOTIN } from "../../lib"
 
 export const CSequence: EditSequence = ({
@@ -5,6 +6,7 @@ export const CSequence: EditSequence = ({
   value,
   onValueChanged,
 }) => {
+  const [inDrag, setInDrag] = useState<number | null>(null)
   const addItem = () => {
     const curr = value !== NOTIN ? value.slice() : []
 
@@ -18,28 +20,54 @@ export const CSequence: EditSequence = ({
     onValueChanged([...curr])
   }
 
+  const handleDragStart = (idx: number) => {
+    setInDrag(idx)
+  }
+
+  const handleDragEnter = (idx: number) => {
+    const newList = value === NOTIN ? [] : [...value]
+    const draggedElement = newList[inDrag!]
+
+    newList.splice(inDrag!, 1)
+    newList.splice(idx, 0, draggedElement)
+
+    setInDrag(idx)
+    onValueChanged(newList)
+  }
+
+  const handleDragEnd = () => {
+    setInDrag(null)
+  }
+
   return (
-    <div className="text-gray-50">
+    <ul className="text-gray-50">
       {innerComponents.map((item, idx) => (
-        <div
-          key={idx}
-          className="flex flex-col border-[1px] border-dashed my-1"
+        <li
+          draggable
+          onDragStart={() => handleDragStart(idx)}
+          onDragEnter={() => handleDragEnter(idx)}
+          onDragEnd={handleDragEnd}
         >
-          <div className="flex flex-row gap-2 items-center">
-            <span>Item {idx + 1}</span>
-            <button
-              className="border-[1px] rounded p-1 text-red-600"
-              onClick={() => removeItem(idx)}
-            >
-              delete
-            </button>
+          <div
+            key={idx}
+            className="flex flex-col border-[1px] border-dashed my-1"
+          >
+            <div className="flex flex-row gap-2 items-center">
+              <span>Item {idx + 1}</span>
+              <button
+                className="border-[1px] rounded p-1 text-red-600"
+                onClick={() => removeItem(idx)}
+              >
+                delete
+              </button>
+            </div>
+            {item}
           </div>
-          {item}
-        </div>
+        </li>
       ))}
       <button className="border-[1px] rounded p-1" onClick={addItem}>
         + Add new item
       </button>
-    </div>
+    </ul>
   )
 }
--- examples/scale-components/src/Codecs/components/EditCodec/CTuple.tsx
+++ examples/scale-components/src/Codecs/components/EditCodec/CTuple.tsx
@@ -2,10 +2,13 @@ import { EditTuple } from "../../lib"
 
 export const CTuple: EditTuple = ({ innerComponents }) => {
   return (
-    <ul className="flex flex-col ml-5">
-      {innerComponents.map((jsx) => (
-        <li>{jsx}</li>
-      ))}
-    </ul>
+    <>
+      <span>Tuple</span>
+      <ul className="flex flex-col ml-5">
+        {innerComponents.map((jsx) => (
+          <li>{jsx}</li>
+        ))}
+      </ul>
+    </>
   )
 }
